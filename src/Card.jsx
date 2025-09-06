cba97b907a57bc0dfabc842592f139e04b354046
Abdullah Hatim <182906341+AbdullahHatim@users.noreply.github.com>
2025-09-05 19:57:27 -0700

    Feat: Finish Initial Styling

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,8 +1,14 @@
 export function Card({id, image}) {
+  const title = image.title.split(' ');
   return (
-    <div data-name="card" data-id={id} className="Card">
-      <img src={image.url} alt="Giphy GIF"/>
-      <p>{image.title.split(' ')[0]}</p>
+    <div data-name="card" data-id={id} className="card">
+      {/* <img src={image.url} alt="Giphy GIF"/> */}
+      <div className="img" style={
+        {
+          backgroundImage: `url(${image.url})`
+        }
+      }></div>
+      <p>{title[0]} {title[1]} {title[2]}</p>
     </div>
   )
 }
--- src/Header.jsx
+++ src/Header.jsx
@@ -4,8 +4,8 @@ export function Header({score}){
       <h1>Memory Game</h1>
       <h2>Click on All cards that you haven't Clicked Before!</h2>
       <div className="score">
-        <div className="current-score">{score.best}</div>
-        <div className="best-score">{score.current}</div>
+        <div className="current-score">Best: {score.best}</div>
+        <div className="best-score">Score: {score.current}</div>
       </div>
     </header>
   )
